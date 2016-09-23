45a7b2affaa854660deeb9c513af99411debde29
pthapa <parmeshwor11@gmail.com>
2016-09-23 16:26:56 -0500

    created models customer, order and menuitem

--- ops/orders/admin.py
+++ ops/orders/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-from .models import Customer, Order
+from .models import Customer, Order, MenuItem
+from .forms import OrderForm
 
 # Register your models here.
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('name','order',)
+    fields = ('name',)
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display= ('name',)
+    fields= ('name','customer','menuitems')
+    form = OrderForm
+
+class MenuItemAdmin(admin.ModelAdmin):
+    fields = ('name','order')
 
 admin.site.register(Customer,CustomerAdmin)
 admin.site.register(Order,OrderAdmin)
+admin.site.register(MenuItem,MenuItemAdmin)
--- ops/orders/models.py
+++ ops/orders/models.py
@@ -3,21 +3,31 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-class Order(models.Model):
-    name = models.CharField(max_length=100)
-    #customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
-    #url = models.UrlField(max_length=200)
-
-    def __str(self):
-        return (str(self.name))
-
-
 class Customer(models.Model):
     name = models.CharField(max_length=100)
     #url = models.UrlField(max_length=200)
-    order = models.ForeignKey(Order,on_delete=models.CASCADE)
 
     def __str__(self):
         return(str(self.name))
+
+class Order(models.Model):
+    name = models.CharField(max_length=100)
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (str(self.name))
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=50)
+    order = models.ManyToManyField(Order,blank=True,null=True,related_name='menuitems')
+
+    def __str__(self):
+        return (str(self.name))
+
+
+
+
+
+
+
 
