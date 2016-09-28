005e1c50d586a6a0f991bc4ab5fa233be2c4eb6b
pthapa <parmeshwor11@gmail.com>
2016-09-28 10:41:00 -0500

    merge prep

--- ops/orders/forms.py
+++ ops/orders/forms.py
@@ -1,30 +1,30 @@
-from django import forms
-from .models import MenuItem, Customer
+# from django import forms
+# from .models import MenuItem, Customer
 
-class OrderForm(forms.ModelForm):
-    menuitems = forms.ModelMultipleChoiceField(
-        MenuItem.objects.all(),
-        required=True
-        )
-    def __init__(self,*args,**kwargs):
-        super(OrderForm,self).__init__(*args,**kwargs)
-        if self.instance.pk:
-            self.initial['menuitems']= self.instance.menuitems.values_list('pk',flat=True)
+# class OrderForm(forms.ModelForm):
+#     menuitems = forms.ModelMultipleChoiceField(
+#         MenuItem.objects.all(),
+#         required=True
+#         )
+#     def __init__(self,*args,**kwargs):
+#         super(OrderForm,self).__init__(*args,**kwargs)
+#         if self.instance.pk:
+#             self.initial['menuitems']= self.instance.menuitems.values_list('pk',flat=True)
 
-    def save (self,*args,**kwargs):
-        instance = super(OrderForm,self).save(*args,**kwargs)
-        if instance.pk:
-            for item in instance.menuitems.all():
-                if item not in self.cleaned_data['menuitems']:
-                    instance.menuitems.remove(item)
-            for item in self.cleaned_data['menuitems']:
-                if item not in instance.menuitems.all():
-                    instance.menuitems.add(item)
-        return instance
+#     def save (self,*args,**kwargs):
+#         instance = super(OrderForm,self).save(*args,**kwargs)
+#         if instance.pk:
+#             for item in instance.menuitems.all():
+#                 if item not in self.cleaned_data['menuitems']:
+#                     instance.menuitems.remove(item)
+#             for item in self.cleaned_data['menuitems']:
+#                 if item not in instance.menuitems.all():
+#                     instance.menuitems.add(item)
+#         return instance
 
-class CustomerForm(forms.ModelForm):
-    class Meta:
-        model = Customer
-        fields = [
-            "name",
-        ]
+# class CustomerForm(forms.ModelForm):
+#     class Meta:
+#         model = Customer
+#         fields = [
+#             "name",
+#         ]
--- ops/orders/urls.py
+++ ops/orders/urls.py
@@ -1,17 +1,17 @@
-from django.conf.urls import url
-from django.contrib import admin
-from .views import (
-		home,
-		list_customer,
-		create_customer,
-		update_customer,
-		delete_customer,
-	)
+# from django.conf.urls import url
+# from django.contrib import admin
+# from .views import (
+# 		home,
+# 		list_customer,
+# 		create_customer,
+# 		update_customer,
+# 		delete_customer,
+# 	)
 
-urlpatterns = [
-    url(r'^$', home, name='home'),
-    url(r'^list/', list_customer, name='list_customer'),
-    url(r'^create/', create_customer, name='create_customer'),
-    url(r'^update/', update_customer, name='update_customer'),
-    url(r'^delete/', delete_customer, name='delete_customer'),
-]
+# urlpatterns = [
+#     url(r'^$', home, name='home'),
+#     url(r'^list/', list_customer, name='list_customer'),
+#     url(r'^create/', create_customer, name='create_customer'),
+#     url(r'^update/', update_customer, name='update_customer'),
+#     url(r'^delete/', delete_customer, name='delete_customer'),
+# ]
